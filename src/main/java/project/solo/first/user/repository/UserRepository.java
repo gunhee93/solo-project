04aa07e1cb2d902535132a77690c4ef960f3dcca
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-07-04 23:20:33 +0900

    feat

--- src/main/java/project/solo/first/user/repository/UserRepository.java
+++ src/main/java/project/solo/first/user/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package project.solo.first.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 import project.solo.first.user.domain.User;
 
