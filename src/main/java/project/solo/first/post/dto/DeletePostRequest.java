c6bd6d48f12e33fbbd8969188ddfd713bdd72a73
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-07-20 22:11:48 +0900

    feat: post
    게시글 삭제

---
+++ src/main/java/project/solo/first/post/dto/DeletePostRequest.java
@@ -0,0 +1,11 @@
+package project.solo.first.post.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DeletePostRequest {
+
+    private Long postId;
+}
--- src/main/java/project/solo/first/post/service/PostService.java
+++ src/main/java/project/solo/first/post/service/PostService.java
@@ -3,8 +3,12 @@ package project.solo.first.post.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import project.solo.first.common.code.ErrorCode;
+import project.solo.first.common.exception.CustomIllegalStateException;
+import project.solo.first.jwt.TokenProvider;
 import project.solo.first.post.domain.Category;
 import project.solo.first.post.domain.Post;
 import project.solo.first.post.dto.*;
@@ -16,6 +20,7 @@ import project.solo.first.user.service.UserService;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,6 +31,7 @@ public class PostService {
     private final CategoryRepository categoryRepository;
     private final PostRepository postRepository;
     private final UserService userService;
+    private final TokenProvider tokenProvider;
 
 
     // 게시글 작성 (카테고리를 저장하는 과정에서 쿼리가 여러번 나가는 문제 해결 필요)
@@ -68,5 +74,18 @@ public class PostService {
         )).collect(Collectors.toList());
 
         return new LikesListResponse(likesPostsDtoList);
+    }
+
+    public void deletePost(String acTokenRequest, DeletePostRequest deletePostRequest) {
+        String accessToken = acTokenRequest.substring(7);
+        Authentication authentication = tokenProvider.getAuthentication(accessToken);
+        String strUserId = authentication.getName();
+        Long userId = Long.parseLong(strUserId);
+
+        Post post = postRepository.findById(deletePostRequest.getPostId()).orElseThrow(() -> {
+            throw new CustomIllegalStateException(ErrorCode.NOT_FOUND_POST);
+        });
+
+        postRepository.delete(post);
     }
 }
